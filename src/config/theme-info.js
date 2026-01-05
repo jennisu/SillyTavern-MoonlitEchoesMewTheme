4718f8961437177d8b0f66234ce79cbdf71ca51b
jennisu <jenny.24091@gmail.com>
2026-01-05 22:17:49 +0100

    Rename extension to 'Mew Moonlit Echoes Theme'

--- src/config/theme-info.js
+++ src/config/theme-info.js
@@ -1,4 +1,4 @@
-export const EXTENSION_NAME = 'Moonlit Echoes Theme';
-export const EXTENSION_ID = 'SillyTavern-MoonlitEchoesTheme';
+export const EXTENSION_NAME = 'Mew Moonlit Echoes Theme';
+export const EXTENSION_ID = 'SillyTavern-MewMoonlitEchoesTheme';
 export const EXTENSION_FOLDER_PATH = `scripts/extensions/third-party/${EXTENSION_ID}`;
 export const THEME_VERSION = '3.0.0';
